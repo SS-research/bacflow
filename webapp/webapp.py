f738567761109ee071251431a34d50ba6b1e0a83
DiTo97 <fede97.minutoli@gmail.com>
2024-10-10 15:03:09 +0200

    perf: drink datetime and simulation range

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,7 +1,9 @@
+from bisect import insort
 import streamlit as st
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 from helper import calc_bac_ts
+from schemas import Drink
 
 # Load drink information
 drink_info = pd.read_csv("dataset.csv")
@@ -20,6 +22,7 @@ st.sidebar.header("Add a drink")
 drink_type = st.sidebar.selectbox("Drink type", drink_info['drink'])
 volume = st.sidebar.slider("Volume (cl)", 1, 120, int(drink_info[drink_info['drink'] == drink_type]['volume'].iloc[0]))
 alc_perc = st.sidebar.slider("Percent alcohol", 0, 100, int(drink_info[drink_info['drink'] == drink_type]['alc_prop'].iloc[0] * 100))
+drink_time_str = st.sidebar.text_input("Time of consumption (YYYY-MM-DD HH:MM)", placeholder=datetime.now().astimezone().strftime("%Y-%m-%d %H:%M"))
 add_drink = st.sidebar.button("Add drink")
 
 # Initialize session state for drinks
@@ -28,29 +31,32 @@ if 'drinks' not in st.session_state:
 
 # Add drink to session state
 if add_drink:
-    drink_time = datetime.now().astimezone()
-    if drink_time is not None:
-        st.session_state.drinks.append({
-            'name': drink_type,
-            'vol': volume / 100,
-            'alc_prop': alc_perc / 100,
-            'time': drink_time.timestamp()
-        })
+    try:
+        if drink_time_str:
+            drink_time = datetime.strptime(drink_time_str, "%Y-%m-%d %H:%M").astimezone()
+        else:
+            drink_time = datetime.now().astimezone()
+
+        insort(st.session_state.drinks, Drink(name=drink_type, vol=volume / 100, alc_prop=alc_perc / 100, time=drink_time), key=lambda x: x.time)
+    except ValueError:
+        st.sidebar.error("Incorrect time format. Please use YYYY-MM-DD HH:MM.")    
+
 
 # Display added drinks
 st.header("Drunken drinks")
 for drink in st.session_state.drinks:
-    st.write(f"{drink['name']} - {drink['vol']*100} cl, {drink['alc_prop']*100}% at {datetime.fromtimestamp(drink['time']).strftime('%H:%M')}")
+    st.write(f"{drink.name} - {drink.vol*100} cl, {drink.alc_prop*100}% at {drink.time.strftime('%H:%M')}")
 
 # Calculate BAC
 if st.session_state.drinks:
-    start_time = min(drink['time'] for drink in st.session_state.drinks)
-    end_time = start_time + 60 * 60 * 24
+    start_time = min(drink.time for drink in st.session_state.drinks)
+    end_time = max(drink.time for drink in st.session_state.drinks) + timedelta(seconds=60 * 60 * 24)
+
     bac_ts = calc_bac_ts(st.session_state.drinks, height / 100, weight, sex, absorption_halflife, beta, start_time, end_time)
 
     st.header("BAC over time")
     st.write("N.B. - The BAC percentage is equivalent to g/dL.")
-    st.line_chart(bac_ts.set_index('time')['bac_perc'], x_label="time (h)", y_label="BAC (%)")
+    st.line_chart(bac_ts.set_index('time')['bac_perc'], x_label="time", y_label="BAC (%)")
 else:
     st.write("No drinks added yet.")
 
