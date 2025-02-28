aa1545f980cb554140dcf9beba34b18bec5d7e37
Federico Minutoli <40361744+DiTo97@users.noreply.github.com>
2025-02-28 21:05:50 +0100

    refactor: model enum in modeling

--- bacflow/bacflow/modeling.py
+++ bacflow/bacflow/modeling.py
@@ -3,71 +3,68 @@ from datetime import datetime, timedelta
 import numpy as np
 import pandas as pd
 
-from schemas import Drink, Person, Sex
+from schemas import Drink, Model, Person, Sex
 
 
-simulation_M = ["forrest", "seidl", "widmark", "ulrich", "watson", "average"]
-simulation_F = ["forrest", "seidl", "widmark", "watson", "average"]
+simulation_M = [Model.Forrest, Model.Seidl, Model.Widmark, Model.Watson, Model.average, Model.Ulrich]
+simulation_F = [Model.Forrest, Model.Seidl, Model.Widmark, Model.Watson, Model.average]
 
 
 class UnsupportedModelException(Exception):
-    def __init__(self, model: str, sex: Sex):
+    def __init__(self, model: Model, sex: Sex):
         self.message = f"The model {model} is not supported for subjects of sex {sex}"
 
     def __str__(self):
         return self.message
 
 
-def calc_body_factor(person: Person, model: str):
-    """Volume distribution of alcohol (VDA) in the body."""
+def _F_calculate_body_factor(person: Person, model: str) -> float: ...
     if person.sex == Sex.F and model not in simulation_F:
         raise UnsupportedModelException(model, person.sex)
 
-    if person.sex == Sex.M and model not in simulation_M:
-        raise UnsupportedModelException(model, person.sex)
-    
     age, height, weight = person.age, person.height, person.weight
 
     match model:
-        case "forrest":
-            r_F = 0.8736 - 0.0124 * weight / height**2
-            r_M = 1.0178 - 0.012127 * weight / height**2
-        case "seidl":
-            r_F = 0.31223 - 0.006446 * weight + 0.4466 * height
-            r_M = 0.31608 - 0.004821 * weight + 0.4632 * height
-        case "widmark":
-            r_F = 0.55
-            r_M = 0.68
-        case "ulrich":
-            r_F = 0.0
-            r_M = 0.715 - 0.00462 * weight + 0.22 * height
-        case "watson":
-            r_F = 0.29218 + (12.666 * height - 2.4846) / weight
-            r_M = 0.39834 + (12.725 * height - 0.11275 * age + 2.8993) / weight
-        case "average":
-            r_F = (
-                0.50766
-                + 0.11165 * height
-                - weight * (0.001612 + 0.0031 / height**2)
-                - (1 / weight) * (0.62115 - 3.1665 * height)
-            )
-            r_M = (
-                0.62544
-                + 0.13664 * height
-                - weight * (0.00189 + 0.002425 / height**2)
-                + (1 / weight) * (0.57986 + 2.545 * height - 0.02255 * age)
-            )
-        case _:
-            raise ValueError(f"Model '{model}' is not supported")
+        case Model.Forrest:
+            r = 0.8736 - 0.0124 * weight / height**2
+        case Model.Seidl:
+            r = 0.31223 - 0.006446 * weight + 0.4466 * height
+        case Model.Widmark:
+            r = 0.55
+        case Model.Watson:
+            r = 0.29218 + (12.666 * height - 2.4846) / weight
+        case Model.average:
+            r = 0.50766 + 0.11165 * height - weight * (0.001612 + 0.0031 / height**2) - (1 / weight) * (0.62115 - 3.1665 * height)
 
-    r_F = np.clip(r_F, 0.44, 0.8)
-    r_M = np.clip(r_M, 0.60, 0.87)
+    return np.clip(r, 0.44, 0.8)
 
-    if person.sex == Sex.F:
-        return r_F
 
-    if person.sex == Sex.M:
-        return r_M
+def _M_calculate_body_factor(person: Person, model: str) -> float:
+    if person.sex == Sex.M and model not in simulation_M:
+        raise UnsupportedModelException(model, person.sex)
+
+    age, height, weight = person.age, person.height, person.weight
+
+    match model:
+        case Model.Forrest:
+            r = 1.0178 - 0.012127 * weight / height**2
+        case Model.Seidl:
+            r = 0.31608 - 0.004821 * weight + 0.4632 * height
+        case Model.Widmark:
+            r = 0.68
+        case Model.Watson:
+            r = 0.39834 + (12.725 * height - 0.11275 * age + 2.8993) / weight
+        case Model.average:
+            r = 0.62544 + 0.13664 * height - weight * (0.00189 + 0.002425 / height**2) + (1 / weight) * (0.57986 + 2.545 * height - 0.02255 * age)
+        case Model.Ulrich:
+            r = 0.715 - 0.00462 * weight + 0.22 * height
+
+    return np.clip(r, 0.60, 0.87)
+
+
+def calculate_body_factor(person: Person, model: str) -> float:
+    """body factor $r$ of subjects with a model of the Widmark family"""
+    return _M_calculate_body_factor(person, model) if person.sex == Sex.M else _F_calculate_body_factor(person, model)
 
 
 def calc_aer(sex: Sex, bac: float) -> float:
@@ -91,10 +88,10 @@ def calc_aer(sex: Sex, bac: float) -> float:
 def calculate_bac_for_model(
     person: Person
     absorption: pd.DataFrame, 
-    model: str,
+    model: Model,
     absorption_end_idx: int
 ) -> tuple[pd.DataFrame, int]:
-    r = calc_body_factor(person, model)
+    r = calculate_body_factor(person, model)
 
     model_bac_ts = absorption.copy()
     model_bac_ts['bac_excluding_elimination'] = model_bac_ts['kg_absorbed'] / (r * person.weight)
